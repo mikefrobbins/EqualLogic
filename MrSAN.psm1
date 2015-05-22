811e808e082cddaea910859d5b04a26fcc994fd0
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2015-05-22 10:20:08 -0500

    Initial Commit of the MrSAN module to GitHub
    Initial commit of MrSAN PowerShell script module v1.0 containing one
    function: Get-MrSANFirmwareVersion and a module
    manifest file.

---
+++ MrSAN.psm1
@@ -0,0 +1,27 @@
+﻿#Requires -Version 3.0
+function Get-MrSANFirmwareVersion {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [string[]]$GroupAddress,
+        
+        [Parameter(Mandatory)]
+        [System.Management.Automation.Credential()]$Credential 
+    )
+
+    BEGIN {
+        Import-Module "$env:ProgramFiles\EqualLogic\bin\EqlPSTools.dll"
+    }
+
+    PROCESS {
+        foreach ($Group in $GroupAddress) {
+            Connect-EqlGroup -GroupAddress $Group -Credential $Credential | Out-Null
+
+            Get-EqlMember -GroupAddress $Group |
+            Select-Object MemberName, FirmwareVersion
+
+            Disconnect-EqlGroup -GroupAddress $Group | Out-Null
+        }
+    }
+}
